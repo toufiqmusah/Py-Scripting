05c1151b317d2f73bd216d2a2e118e461a9daf9b
Toufiq <92761339+toufiqmusah@users.noreply.github.com>
2022-11-22 18:31:14 +0000

    First Commit

---
+++ Script_py.py
@@ -0,0 +1,50 @@
+""""
+import sys
+
+print(sys.argv)
+print(sys.version)
+"""
+
+'''
+import os
+
+print(os.getcwd()) #Get Current Directory
+os.chdir("C:\\Users\OWNER\\Downloads") #Changes directory to file path
+print(os.getcwd())
+
+#os.mkdir("C:\\Users\OWNER\\Downloads\\AREPO") #Creates a directory
+#os.rmdir("C:\\Users\OWNER\\Downloads\\AREPO") #Deletes a directory. os.remove("") also works for files
+
+#print(os.path.join("C:\\Users\\OWNER\Desktop\\PyKIDS", "C:\\Users\\OWNER\\Desktop\\Scriipt_py")) #Joins paths
+#print(os.path.split("C:\\Users\\OWNER\Desktop\\PyKIDS", "C:\\Users\\OWNER\\Desktop\\Scriipt_py")) #Splits paths
+print(os.path.exists("C:\\Users\\OWNER\\Desktop\\Scriipt_py")) #Checks if a path exists, returning True or False
+'''
+
+
+"""
+import math
+
+print(math.pi)
+print(math.e)
+print(math.degrees(0.2))
+print(math.cos(60))
+print(math.tanh(18*math.pi))
+print(math.exp(3)) #i.e e to the power 3
+print(math.log(5,10)) #i.e 5 base 10 logarithm
+
+"""
+"""
+import random
+
+print(random.randrange(0,80,10)) #the last parameter is the step size
+"""
+
+"""
+import datetime
+
+print(datetime.date.today())
+
+now = datetime.datetime.today()
+other = datetime.datetime(2001, 1, 5, 8, 10)
+
+# print(now-other) """
